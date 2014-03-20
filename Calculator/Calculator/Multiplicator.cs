8245557baf5847aa31bf4a5f92e548d09f2b3ce2
Neals <niceneals@mail.ru>
2014-03-20 10:29:19 +0700

    Added classes for basic operations;

---
+++ Calculator/Calculator/Multiplicator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Multiplicator
+    {
+        public string Calculate(double first, double second)
+        {
+            return (first * second).ToString();
+        }
+    }
+}
---
+++ Calculator/Calculator/Substructor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Substructor
+    {
+        public string Calculate (double first, double second)
+        {
+            return (first - second).ToString();
+        }
+    }
+}
