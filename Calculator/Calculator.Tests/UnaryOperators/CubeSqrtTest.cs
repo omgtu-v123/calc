5d605b5e848d450e9287672962b6f799f92f27da
DenisQa1337 <moiseev1995@mail.ru>
2014-04-10 10:35:12 +0700

    Added more tests for unary operations;

---
+++ Calculator/Calculator.Tests/UnaryOperators/CubeSqrtTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.UnaryOperators;
+using NUnit.Framework;
+
+namespace Calculator.Tests.UnaryOperators
+{
+    public class CubeSqrtTest
+    {
+        [TestFixture]
+        public class CubeSqrtTests
+        {
+            [Test]
+            public void CalculateTest()
+            {
+                CubeSqrt cubesqrt = new CubeSqrt();
+                Assert.AreEqual("3", cubesqrt.Calculate(27), "cube sqrt (27) != 3");
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/UnaryOperators/Sqrt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.UnaryOperators;
+using NUnit.Framework;
+
+namespace Calculator.Tests.UnaryOperators
+{
+    public class SqrtTest
+    {
+        [TestFixture]
+        public class SqrtTests
+        {
+            [Test]
+            public void CalculateTest()
+            {
+                Sqrt sqrt = new Sqrt();
+                Assert.AreEqual("3", sqrt.Calculate(9), "sqrt (9) != 3");
+            }
+        }
+    }
+}
