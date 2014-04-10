a968ea3b73684a8d2209fe6acfc808327babd17f
Belyanin /Neals/ <m.viloev@gmail.com>
2014-04-10 11:28:38 +0700

    Added more tests for unary operations;

---
+++ Calculator/Calculator.Tests/UnaryOperators/TangenterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.UnaryOperators;
+using NUnit.Framework;
+
+namespace Calculator.Tests.UnaryOperators
+{
+    public class TangenterTest
+    {
+        [TestFixture]
+        public class TangenterTests
+        {
+            [Test]
+            public void CalculateTest()
+            {
+                Tangenter tangenter = new Tangenter();
+                Assert.AreEqual(-3.3805, tangenter.Calculate(5), 0.0001, "tangenter != 0");
+            }
+        }
+    }
+}
