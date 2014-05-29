a929dfb93bd875caedc167521da2ac81a8db53b1
DenisQa1337 <moiseev1995@mail.ru>
2014-05-29 10:09:40 +0700

    Added test for bubble method;

---
+++ Calculator/Calculator.Tests/SortingOperations/BubbleMethodTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.SortingOperations;
+using NUnit.Framework;
+
+namespace Calculator.Tests.SortingOperations
+{
+    [TestFixture]
+    public class BubbleMethodTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            BubbleMethod bubbleMethod = new BubbleMethod();
+            int[] example = {1, 2, 3};
+            int[] data = {2, 1, 3};
+            Assert.AreEqual(example, bubbleMethod.SortingCalculate(data), "Sorting operation do not working");
+        }
+    }
+}
